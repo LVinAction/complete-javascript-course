13deccb74e8d7ea891a847303360d024acbf1021
LVinAction <68782090+LVinAction@users.noreply.github.com>
2024-08-16 15:55:22 -0400

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,30 @@
+let country = "USA";
+let continent = "North America";
+let population = "360";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+let isIsland = false;
+let language ;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = "English";
+
+console.log(population / 2);
+population++;
+
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+
+const description = country + " is in " + continent + ", and its " + population + " million people speak " + language +".";
+
+console.log(description);
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,83 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = "programmer";
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+//const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Villalobos";
+console.log(lastName);
+*/
+
+
+
+
+
+
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2*2*2
+
+const firstName = "Jonas";
+const lastName = "Schedtmann";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
