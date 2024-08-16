c4017a261f1c883d22a8a8dc358c70cee8cb8f69
LVinAction <68782090+LVinAction@users.noreply.github.com>
2024-08-16 17:20:46 -0400

    Add files via upload

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -60,18 +60,11 @@ lastName = "Villalobos";
 console.log(lastName);
 */
 
-
-
-
-
-
-
-
+//Math Operators
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
-
 
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 // 2 ** 3 means 2 to the power of 3 = 2*2*2
@@ -80,4 +73,19 @@ const firstName = "Jonas";
 const lastName = "Schedtmann";
 console.log(firstName + " " + lastName);
 
+//Assignment Operators
 let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x -  1 = 100
+x--; // x = x - 1 = 99
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
