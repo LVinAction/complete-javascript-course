c57bdbac6bf278bf2de3a29c0b76aa415b3b9290
LVinAction <68782090+LVinAction@users.noreply.github.com>
2024-09-09 17:47:16 -0400

    9/9/24 Course Work

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -42,3 +42,27 @@ function describePopulation(country, population) {
 describePopulation("Portugal", 10);
 describePopulation("China", 1441);
 describePopulation("USA", 332);
+
+const populations = [10, 1441, 332, 83];
+console.log(populations.length === 4);
+const percentages = [
+  percentageOfWorld1(populations[0]),
+  percentageOfWorld1(populations[1]),
+  percentageOfWorld1(populations[2]),
+  percentageOfWorld1(populations[3]),
+];
+console.log(percentages);
+
+const neighbours = ["Canada", "Mexico", "Russia"];
+neighbours.push("Utopia");
+console.log(neighbours);
+neighbours.pop();
+console.log(neighbours);
+
+if (!neighbours.includes("Germany")) {
+  console.log(`'Probably not a central european country :D'`);
+}
+
+console.log(neighbours.indexOf("Mexico"));
+neighbours[1] = "Republic of Mexico";
+console.log(neighbours);
